434447b00b447938bc7312cf9b53a6b72bc92f48
karim-sebih <karim.sebih@laplateforme.io>
2025-01-13 13:30:22 +0100

    job1

---
+++ jour1/job01/job1.php
@@ -0,0 +1,25 @@
+<?php
+
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+
+echo $str2 . " " . $str . " " . $str3 ;
+
+
+$val = 6;
+
+echo $val. "\n";
+
+$val += 4;
+
+echo $val."\n";
+
+
+$myBool =true;
+
+echo $myBool;
+
+$myBool =false;
+
+echo $myBool;
