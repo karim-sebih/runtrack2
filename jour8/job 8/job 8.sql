96185250ca942443fca3bd6cc69dbf5618dd84dd
karim-sebih <karim.sebih@laplateforme.io>
2025-01-23 15:08:09 +0100

    jour 8 job 5 a 14

--- jour8/job 8/job 8.sql
+++ jour8/job 8/job 8.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 23 jan. 2025 à 12:08
+-- Généré le : jeu. 23 jan. 2025 à 14:22
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -40,14 +40,8 @@ CREATE TABLE `etudiants` (
 -- Déchargement des données de la table `etudiants`
 --
 
-INSERT INTO `etudiants` (`id`, `prenom`, `nom`, `naissance`, `sexe`, `email`) VALUES
-(1, 'Cyril', 'Zimmermann', '1989-01-02', 'masculin', 'cyril@laplateforme.io'),
-(2, 'Jessica', 'Soriano', '1995-09-08', 'feminin', 'jessica@laplateforme.io'),
-(3, 'Roxan', 'Roumégas ', '2016-09-08', 'masculin', 'roxan@laplateforme.io'),
-(4, 'Pascal', 'Assens', '1999-12-31', 'masculin', 'pascal@laplateforme.io'),
-(5, 'Terry', 'Cristinelli', '2005-02-01', 'masculin', 'terry@laplateforme.io'),
-(6, 'Ruben', 'Habib\r\n', '1993-05-26', 'masculin', 'ruben.habib@laplateforme.io'),
-(7, '➔ Toto,', ' Dupont', '2019-11-07', 'masculin', 'toto@laplateforme.io');
+INSERT INTO `etudiants` (`COUNT(id)`) VALUES
+(7);
 
 --
 -- Index pour les tables déchargées
